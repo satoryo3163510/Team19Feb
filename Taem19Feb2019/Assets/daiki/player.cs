9ebe49abd3d6fe3934ff6d688516605f69e83dea
KPC-017A1390\K-017A1390 <dai0603kmy743@icloud.com>
2019-04-12 16:02:45 +0900

    プレイヤーの動作

---
+++ Taem19Feb2019/Assets/daiki/player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float speed = 0.5f;
+
+    private Vector3 Player_pos;
+    private float x;
+    private float y;
+    
+
+    void Start()
+    {
+        Player_pos = GetComponent<Transform>().position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.AddForce(x * speed, 0, z * speed);
+
+        Vector3 diff = transform.position - Player_pos;
+        if (diff.magnitude > 0.01f)
+        {
+            transform.rotation = Quaternion.LookRotation(diff);
+        }
+
+        Player_pos = transform.position;
+    }
+
+        
+    
+}

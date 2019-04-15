76a25fa31a2836ee15493556a658bbcaea700d46
KPC-017A1390\K-017A1390 <dai0603kmy743@icloud.com>
2019-04-15 16:12:26 +0900

    ミサイル艦プログラム途中

---
+++ Taem19Feb2019/Assets/MisaillShipattack.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MisaillShipattack : MonoBehaviour
+{
+    //発射間隔調整
+    public GameObject shellPrefad;
+    public float shotSpeed;
+    public AudioClip shotSound;
+
+    private float timeVeweenShot = 0.35f;
+    private float timer;
+    //ホーミング
+    public Transform target;
+    public Vector3 velcoity;
+    public Vector3 position;
+    public float period;
+
+    public float maxAcceleration = 100;
+
+    
+    public float randomPower;
+    public float randomPeriod;
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ホーミングミサイルプログラム
+        if (target == null)
+            return;
+
+        var acceleration = Vector3.zero;
+        var diff = target.position - position;
+
+        acceleration += (diff - velcoity * period) * 2f / (period * period);
+
+        if (0 < randomPeriod)
+        {
+            var xr = Random.Range(-randomPower, randomPower);
+             
+            var yr = Random.Range(-randomPower, randomPower);
+
+            var zr = Random.Range(-randomPower, randomPower);
+            acceleration += new Vector3(xr, yr, zr);
+        }
+
+        if (acceleration.magnitude > maxAcceleration)
+        {
+            acceleration = acceleration.normalized * maxAcceleration;
+        }
+
+        period -= Time.deltaTime;
+        randomPeriod -= Time.deltaTime;
+
+        if (period < 0f) 
+        return;
+        velcoity += acceleration * Time.deltaTime;
+        position += velcoity * Time.deltaTime;
+        transform.position = position;
+
+        //発射間隔プログラム
+        timer += Time.deltaTime;
+
+        
+        
+    }
+}
--- Taem19Feb2019/Assets/daiki/player.cs
+++ Taem19Feb2019/Assets/daiki/player.cs
@@ -2,39 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController),typeof(AudioSource))]
+
 public class player : MonoBehaviour
 {
-    public float speed = 0.5f;
+    [SerializeField]
+    private string fire1String;
+
+    [SerializeField]
+    public int pnString;
+
+    public float speed = 5f;
+
+    public float countTime;
+    float count = 0f;
 
     private Vector3 Player_pos;
     private float x;
-    private float y;
+    private float z;
+    private Rigidbody rigid;
     
 
     void Start()
     {
+        GetComponent<Rigidbody>().rotation = Quaternion.identity;
+
         Player_pos = GetComponent<Transform>().position;
+
+        rigid = GetComponent<Rigidbody>();
+        //transform.position -= new Vector3(0, 0, 0);
+        //gameObject.GetComponent<player>().enabled = true;
+        //count = 0;
+        //countTime = 4;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+     void FixedUpdate()
     {
         float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        float z = Input.GetAxis("Velotical");
 
+        //（仮）
         Rigidbody rigidbody = GetComponent<Rigidbody>();
-
-        rigidbody.AddForce(x * speed, 0, z * speed);
-
+        rigid.AddForce(x * speed, 0, z * speed);
+        
+        //向いている方向
         Vector3 diff = transform.position - Player_pos;
-        if (diff.magnitude > 0.01f)
+        Rigidbody rd = this.transform.GetComponent<Rigidbody>();
+        rd.velocity = new Vector3(x, 0, z);
+        Vector3 veloctiy = rd.rotation * new Vector3(0, 0, speed);
+
+        countTime -= Time.deltaTime;
+
+        gameObject.transform.position += veloctiy * Time.deltaTime;
+        if (diff.magnitude > 0.0f)
         {
             transform.rotation = Quaternion.LookRotation(diff);
         }
-
         Player_pos = transform.position;
+
+     
     }
 
-        
-    
 }

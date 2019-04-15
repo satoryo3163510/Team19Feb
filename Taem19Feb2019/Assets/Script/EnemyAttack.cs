85e6007d6403d6e7f8e39ac1217df369ac8e3ddb
satoryo3163510 <43367124+satoryo3163510@users.noreply.github.com>
2019-04-15 10:14:29 +0900

    敵艦がプレイヤーに向けて弾を発射するようにした

--- Taem19Feb2019/Assets/Script/EnemyAttack.cs
+++ Taem19Feb2019/Assets/Script/EnemyAttack.cs
@@ -1,22 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    private Transform player;
-    [SerializeField]
-    private GameObject bullet;
+    public  GameObject bullet;
+    private float timer;
+    public Transform muzzle;
+    private float bSpeed = 100;
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player").transform;
+        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 dir = player.position - transform.position;
-        Instantiate(bullet,transform);                    
+        timer += Time.deltaTime;
+        Debug.Log(timer);
+        if (timer >= 0.1)
+        {
+            GameObject bullets = Instantiate(bullet) as GameObject;
+            Vector3 force = gameObject.transform.right * bSpeed;
+            bullets.GetComponent<Rigidbody>().AddForce(force);
+            bullets.transform.position = muzzle.position;
+            Destroy(bullets, 5);
+            timer = 0;
+        }
     }
 }

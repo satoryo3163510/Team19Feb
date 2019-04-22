54d47dc3dca7cce7f8573555a0c0ac59b09adb91
KPC-017A1397\K017A1397 <satonom210@gmail.com>
2019-04-22 16:09:28 +0900

    シールドゲージ（仮）見た目だけ追加、カメラ切り替えは要相談、

--- Taem19Feb2019/Assets/SatouNa/Script/ChangeCamera.cs
+++ Taem19Feb2019/Assets/SatouNa/Script/ChangeCamera.cs
@@ -12,8 +12,8 @@ public class ChangeCamera : MonoBehaviour
     private GameObject playerObject;    //回転の中心となるプレイヤー
     private float rotateSpeed = 0.5f;   //回転の速さ
     public Image sight;                 //ロックオンサイト
-
-
+    public bool changeflag;
+    
     // Start is called before the first frame update
     //開始時は三人称、ScopeModeはオフに
     void Start()
@@ -21,6 +21,7 @@ public class ChangeCamera : MonoBehaviour
         playerObject = GameObject.Find("PlayerSenkan");
         gameObject.GetComponent<ScopeMode>().enabled = false;
         sight.enabled = false;
+        changeflag = true;
     }
 
     // Update is called once per frame
@@ -28,7 +29,7 @@ public class ChangeCamera : MonoBehaviour
     {
         //rotateCameraの呼び出し
         RotateCamera();
-
+        
         //右クリックが押されたらカメラのアクティブを切り替える
         if (Input.GetMouseButtonDown(1))
         {
@@ -37,9 +38,10 @@ public class ChangeCamera : MonoBehaviour
         }
     }
 
+    //一人称の時にマウス操作でカメラと同時に旋回することで
+    //三人称のカメラとの違和感を軽減できる？（保留）
     private void RotateCamera()
     {
-        
         //transform.RtateAround()をしようしてメインカメラを回転させる
         //fpsCameraがactiveのときはsightUIを表示する
         if (mainCamera.activeSelf)
@@ -53,12 +55,11 @@ public class ChangeCamera : MonoBehaviour
         }
         else if (fpsCamera.activeSelf)
         {
-            Vector3 angl = new Vector3(Input.GetAxis("Mouse X") * -rotateSpeed*0.4f, Input.GetAxis("Mouse Y") * -rotateSpeed*0.4f, 0);
+            Vector3 angl = new Vector3(Input.GetAxis("Mouse X") * -rotateSpeed * 0.4f, Input.GetAxis("Mouse Y") * -rotateSpeed * 0.4f, 0);
             fpsCamera.transform.RotateAround(playerObject.transform.position, Vector3.down, angl.x);
             fpsCamera.transform.RotateAround(playerObject.transform.position, transform.right, angl.y);
             gameObject.GetComponent<ScopeMode>().enabled = true;
             sight.enabled = true;
         }
-
     }
 }

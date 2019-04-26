b8d87e659797869d57fd5014ecbde1ac6ba0bd3d
KPC-017A1390\K-017A1390 <dai0603kmy743@icloud.com>
2019-04-26 16:10:45 +0900

    ミサイル艦調整

--- Taem19Feb2019/Assets/daiki/Script/Misaill.cs
+++ Taem19Feb2019/Assets/daiki/Script/Misaill.cs
@@ -4,25 +4,28 @@ using UnityEngine;
 
 public class Misaill : MonoBehaviour
 {
-    //Rigidbodyを入れる
+    //Rigidbodyを入れる変数
     Rigidbody rigid;
     //速度
     Vector3 velocity;
     //発射するときの初期位置
-    public Vector3 position;
-
-    //加速度
+    Vector3 position;
+    // 加速度
     public Vector3 acceleration;
-    //ターゲットをセット
+    // ターゲットをセットする
     public Transform target;
-
-    //着弾時間
+    // 着弾時間
     float period = 2f;
+
+   
+
 
     // Start is called before the first frame update
     void Start()
     {
+        // 初期位置をposionに格納
         position = transform.position;
+        // rigidbody取得
         rigid = this.GetComponent<Rigidbody>();
     }
 
@@ -30,32 +33,44 @@ public class Misaill : MonoBehaviour
     void Update()
     {
         acceleration = Vector3.zero;
-    //ターゲットと自分自身の差
+
+        //ターゲットと自分自身の差
         var diff = target.position - transform.position;
+
         //加速度を求めてるらしい
-        acceleration += (diff - velocity * period) * 2f / (period * period);
+        acceleration += (diff - velocity * period) *1f
+                        / (period * period);
+
         //加速度が一定以上だと追尾を弱くする
         if (acceleration.magnitude > 100f)
         {
-            acceleration = acceleration.normalized * 100f;
+            acceleration = acceleration.normalized * 100;
         }
 
-        //着弾時間を徐々に減らす
+        // 着弾時間を徐々に減らしていく
         period -= Time.deltaTime;
 
-        //速度の計算
+        // 速度の計算
         velocity += acceleration * Time.deltaTime;
+
+
+        
+        
+
     }
 
     void FixedUpdate()
     {
-        //移動処理
+        // 移動処理
         rigid.MovePosition(transform.position + velocity * Time.deltaTime);
-
     }
 
     void OnCollisionEnter()
     {
+        // 何かに当たったら自分自身を削除
         Destroy(this.gameObject);
+
     }
+
+
 }
--- Taem19Feb2019/Assets/daiki/Script/MisaillShipShell.cs
+++ Taem19Feb2019/Assets/daiki/Script/MisaillShipShell.cs
@@ -4,17 +4,42 @@ using UnityEngine;
 
 public class MisaillShipShell : MonoBehaviour
 {
-    float misaillshipShell;
+    public GameObject EnemyBullet;
+   
+    float Z_Speed = 0;
+    float intervalTime;
 
-    // Start is called before the first frame update
+    // 敵が攻撃する間隔
+    float enemyAttackInterval;
+    // 経過時間 
+    float enemyAttackTimer;
+
     void Start()
     {
-        misaillshipShell = Random.Range(5.0f, 10.0f);
+        // enemyAttackIntervalを初期化、5~10の数値が入る
+        enemyAttackInterval = Random.Range(1.0f, 5.0f);
+        Destroy(this.gameObject, 5);
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-            }
-}
+        transform.Translate(0, 0, -1 * Z_Speed);
+        //以下、攻撃用コード
+        Quaternion quat = Quaternion.Euler(0, 180, 0);
+        intervalTime += Time.deltaTime;
+        if (intervalTime >= enemyAttackInterval)
+        {
+            intervalTime = 0.0f;
+            enemyAttackTimer = 0; // タイマーを戻す
+            enemyAttackInterval = Random.Range(5.0f, 10.0f);
+            Instantiate(EnemyBullet, new Vector3(transform.position.x, transform.position.y, transform.position.z), quat);
+        }
+        enemyAttackTimer += Time.deltaTime;
+
+
+    }
+
+   
+
+    }

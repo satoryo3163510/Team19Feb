710779715c90aacc6c14de976f6f2acbbf46e4dc
satoryo3163510 <43367124+satoryo3163510@users.noreply.github.com>
2019-04-26 09:09:49 +0900

    4/24作業分

---
+++ Taem19Feb2019/Assets/Script/BigCannon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigCannon : MonoBehaviour
+{
+    private Transform player;
+    private Vector3 move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("PlayerSenkan").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Playerを見る
+        var aim = player.position - transform.position;
+        var look = Quaternion.LookRotation(aim,Vector3.up);
+        transform.localRotation = look;
+        //transform.LookAt(player);
+    }
+}

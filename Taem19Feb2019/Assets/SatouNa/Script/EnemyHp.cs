47d1965c040df3c337db5bf670e8ed2faaffa242
KPC-017A1397\K017A1397 <satonom210@gmail.com>
2019-05-10 12:09:48 +0900

    enemyHpを製作開始

---
+++ Taem19Feb2019/Assets/SatouNa/Script/EnemyHp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHp : MonoBehaviour
+{
+    //エネミーのHp,消滅関係
+    [SerializeField]
+    private float enemyHp;
+    [SerializeField]
+    private float maxEnemyHp = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyHp = maxEnemyHp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EnemyDamage(float damage)
+    {
+        enemyHp -= damage;
+        if (enemyHp <= 0)
+            Destroy(gameObject);
+    }
+}
--- Taem19Feb2019/Assets/SatouNa/Script/ScopeMode.cs
+++ Taem19Feb2019/Assets/SatouNa/Script/ScopeMode.cs
@@ -13,8 +13,9 @@ public class ScopeMode : MonoBehaviour
     private int zoom1 = 10;                 //拡大倍率（近）
     private int zoom2 = 20;                 //拡大倍率（遠）
     private bool zoomFlag;                  //切り替えフラグ
-    private Vector3 center;                 //画面中央座標
-    
+    private Vector3 center;                //画面中央座標
+    private EnemyHp EH;
+    private GameObject enemy;
 
 
     // Start is called before the first frame update
@@ -23,6 +24,8 @@ public class ScopeMode : MonoBehaviour
         fpsCamera = GetComponent<Camera>();
         fpsCamera.fieldOfView = zoom2;
         center = new Vector3(Screen.width / 2, Screen.height / 2);
+        enemy = GameObject.FindWithTag("Enemy");
+        EH = enemy.GetComponent<EnemyHp>();
     }
 
     // Update is called once per frame
@@ -57,28 +60,29 @@ public class ScopeMode : MonoBehaviour
     void Shoot()
     {
         //実弾処理、エフェクトに転用できるのでコメントアウト
-        //GameObject bullets = Instantiate(bullet) as GameObject;
-        //Vector3 force;
+        GameObject bullets = Instantiate(bullet) as GameObject;
+        Vector3 force;
         //force = this.gameObject.transform.forward * bulletSpeed;
-        //force = fpsCamera.transform.forward * bulletSpeed;
+        force = fpsCamera.transform.forward * bulletSpeed;
 
-        //bullets.GetComponent<Rigidbody>().AddForce(force);
+        bullets.GetComponent<Rigidbody>().AddForce(force);
 
-        //bullets.transform.position = muzzle.position;
+        bullets.transform.position = fpsCamera.transform.position;
 
         //レーザー射撃
         Ray ray = fpsCamera.ScreenPointToRay(center);
         RaycastHit hit;
 
         //rayの可視化用デバッグ
-        Debug.DrawRay(ray.origin, ray.direction*10, Color.green, 5, false);
+        Debug.DrawRay(ray.origin, ray.direction*20, Color.green, 5, false);
 
         //rayがhitした場合
-        if (Physics.Raycast(ray,out hit, 100))
+        if (Physics.Raycast(ray,out hit, 20))
         {
             //destroyの時間差で演出を入れる
-            Destroy(hit.collider.gameObject);
-           
+            //Destroy(hit.collider.gameObject);
+            
+            EH.EnemyDamage(40f);
         }
     }
 }

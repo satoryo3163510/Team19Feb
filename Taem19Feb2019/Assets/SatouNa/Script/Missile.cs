6980a681f90f6ee8e7669ec30342d85f2e32cc2d
KPC-017A1397\K017A1397 <satonom210@gmail.com>
2019-05-10 16:09:35 +0900

    敵のhp完成（仮）、次回ミサイル

---
+++ Taem19Feb2019/Assets/SatouNa/Script/Missile.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    //ミサイル発射プログラム
+    //敵に当たるまで止まることはない
+
+    private Rigidbody rb;
+    [SerializeField]
+    private float speed;
+    private Vector3 force;
+
+    
+    // Use this for initialization
+    void Start()
+    {
+        force = transform.forward * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        rb.AddForce(force);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            //範囲内の敵に必中する。
+        }
+    }
+}

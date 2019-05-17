83287eaa5e4dad4e56b20768fc5bb41a51644ca2
satoryo3163510 <43367124+satoryo3163510@users.noreply.github.com>
2019-05-17 11:31:50 +0900

    commit

--- Taem19Feb2019/Assets/Script/Enemy_shield.cs
+++ Taem19Feb2019/Assets/Script/Enemy_shield.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Enemy_shield : MonoBehaviour
 {
     public GameObject shield;
+    EnemyAll all;
     // Start is called before the first frame update
     void Start()
     {
+        all = new EnemyAll();
         GameObject kyoten = GameObject.Find("Kyoten");
         GameObject kyotenshield = Instantiate(shield,transform) as GameObject;
         kyotenshield.transform.position = kyoten.transform.position;

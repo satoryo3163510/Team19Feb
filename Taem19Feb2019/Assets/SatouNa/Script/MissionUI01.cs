01f08129a8bb3b1142687f60c184bb8fc6748398
KPC-017A1397\K017A1397 <satonom210@gmail.com>
2019-05-20 16:09:06 +0900

    チュートリアル作成開始

---
+++ Taem19Feb2019/Assets/SatouNa/Script/MissionUI01.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MissionUI01 : MonoBehaviour
+{
+    public Text misson01;
+    // Start is called before the first frame update
+    void Start()
+    {
+        misson01.text=("機体のテストを開始します");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //テキスト書き換え
+        
+    }
+}

ddccb7fdf194aa41209cd1a89e00d77f3b7e8df3
satoryo3163510 <43367124+satoryo3163510@users.noreply.github.com>
2019-05-27 16:09:25 +0900

    統合内容ちょっと修正した

--- Taem19Feb2019/Assets/SatouNa/Script/EnemyHp.cs
+++ Taem19Feb2019/Assets/SatouNa/Script/EnemyHp.cs
@@ -11,19 +11,25 @@ public class EnemyHp : MonoBehaviour
     private float maxEnemyHp = 100f;
     public GameObject EF_explode_2;
     public GameObject EF_damage;
-    public bool isDead;
+    new AudioSource audio;
+    [Header("ヒット音")]
+    public AudioClip audioDamage;
+    [Header("爆発音")]
+    public AudioClip audioDestory;
+    public bool isdead;
 
     // hpの初期化
     void Start()
     {
         enemyHp = maxEnemyHp;
-        isDead = false;
+        audio = GetComponent<AudioSource>();
+        isdead = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void EnemyDamage(float damage)
@@ -34,12 +40,18 @@ public class EnemyHp : MonoBehaviour
             var ef_explode2 = Instantiate(EF_explode_2, transform.position, Quaternion.identity);
             Destroy(ef_explode2, 1f);
             Destroy(gameObject, 0.4f);
-            isDead = true;
+            audio.PlayOneShot(audioDestory);
+            isdead = true;
         }
         else
         {
             var ef_damage = Instantiate(EF_damage, transform.position, Quaternion.identity);
-            Destroy(ef_damage,1f);
+            Destroy(ef_damage, 1f);
+            audio.PlayOneShot(audioDamage);
         }
+    }
+    public float Return()
+    {
+        return enemyHp;
     }
 }
